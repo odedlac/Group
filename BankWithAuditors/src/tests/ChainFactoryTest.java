288289a41da6fcbebb783b1b989bea64c17efcda
Oded Lachish <oded@dcs.bbk.ac.uk>
2012-11-16 16:00:45 +0000

    New chain factory test

---
+++ BankWithAuditors/src/tests/ChainFactoryTest.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author oded
+ *
+ */
+public class ChainFactoryTest {
+
+	@Test
+	public void test() {
+		
+		fail("Not yet implemented");
+	}
+
+}
