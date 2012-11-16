a1c79cb175715049407b12c528323336e32acf9b
Oded Lachish <oded@dcs.bbk.ac.uk>
2012-11-16 16:19:29 +0000

    OHandler test works

--- BankWithAuditors/src/tests/OHandlerTest.java
+++ BankWithAuditors/src/tests/OHandlerTest.java
@@ -2,13 +2,31 @@ package tests;
 
 import static org.junit.Assert.*;
 
+
+
 import org.junit.Test;
+
+import code.*;
 
 public class OHandlerTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Handler classUnderTest = new OHandler();
+		Request request = new Request("Oded","5","10");
+		
+		Boolean actualOutput = classUnderTest.handleRequest(request);
+		
+		assertTrue("Handler failed to grab",actualOutput);
 	}
 
+	@Test
+	public void test2() {
+		Handler classUnderTest = new OHandler();
+		Request request = new Request("Ode","5","10");
+		
+		Boolean actualOutput = classUnderTest.handleRequest(request);
+		
+		assertFalse("Handler failed to grab",actualOutput);
+	}
 }
