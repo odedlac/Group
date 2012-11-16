9774ffa8486e7f545b6d1b796b10d4ac3cf05394
Oded Lachish <oded@dcs.bbk.ac.uk>
2012-11-16 16:24:42 +0000

    Added JHandler

--- BankWithAuditors/src/tests/ChainFactoryTest.java
+++ BankWithAuditors/src/tests/ChainFactoryTest.java
@@ -7,6 +7,11 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import code.ChainFactory;
+import code.Handler;
+import code.OHandler;
+import code.Request;
+
 /**
  * @author oded
  *
@@ -15,8 +20,18 @@ public class ChainFactoryTest {
 
 	@Test
 	public void test() {
+		Handler firstHandler = new OHandler();
+		Handler secondHandler = new JHandler();
+		Handler headHandler;
 		
-		fail("Not yet implemented");
+		ChainFactory classUnderTest = new ChainFactory();
+		
+		
+		Request request = new Request("Oded","5","10");
+		
+		Boolean actualOutput = classUnderTest.handleRequest(request);
+		
+		assertTrue("Handler failed to grab",actualOutput);
 	}
 
 }
