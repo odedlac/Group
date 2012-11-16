9fed1fded085e03d3ae99c56f951d12637b41100
Oded Lachish <oded@dcs.bbk.ac.uk>
2012-11-16 15:59:38 +0000

    Added chain factory

--- BankWithAuditors/src/code/Request.java
+++ BankWithAuditors/src/code/Request.java
@@ -2,22 +2,48 @@ package code;
 
 import java.util.Map;
 
-public abstract class Request {
+public class Request {
 
-	Map<String,String> instructions;
+	Map<String, String> instructions;
 
 	
 	private String requestType;
 
+	private String instruction;
+	private String instructionField;
+
+	public Request(String requestType,String inst, String instF) {
+		super();
+		this.requestType = requestType;
+		this.instruction = inst;
+		this.instructionField = instF;
+	}
+
+
 	
-	
-	
+	/**
+	 * @return the instruction
+	 */
+	public String getInstruction() {
+		return instruction;
+	}
+
+
+	/**
+	 * @return the instructionField
+	 */
+	public String getInstructionField() {
+		return instructionField;
+	}
+
+
+	/**
+	 * @return the requestType
+	 */
+
 	public String getRequestType() {
 		return requestType;
 	}
 
-	protected void setRequestType(String requestType) {
-		this.requestType = requestType;
-	}
 	
 }
---
+++ BankWithAuditors/src/tests/RequestTest.java
@@ -0,0 +1,59 @@
+package tests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import code.Request;
+
+public class RequestTest {
+
+	@Test
+	public void test() {
+		
+		String instruction = "10";
+		String instructionField = "5";
+		String expectedOutput = "Oded";
+		String actualOutput;
+		String input = "Oded";
+		Request classUnderTest = new Request(input, instruction, instructionField);
+		
+		actualOutput = classUnderTest.getRequestType();
+		
+			
+		assertEquals("Not equal",expectedOutput,actualOutput);
+	}
+
+	@Test
+	public void test2() {
+		
+		String instruction = "10";
+		String instructionField = "5";
+		String expectedOutput = "10";
+		String actualOutput;
+		String input = "Oded";
+		Request classUnderTest = new Request(input, instruction, instructionField);
+		
+		
+		actualOutput = classUnderTest.getInstruction();
+		
+		assertEquals("Not equal",expectedOutput,actualOutput);
+	}
+
+	@Test
+	public void test3() {
+		
+		String instruction = "10";
+		String instructionField = "5";
+		String expectedOutput = "5";
+		String actualOutput;
+		String input = "Oded";
+		Request classUnderTest = new Request(input, instruction, instructionField);
+		
+		
+		actualOutput = classUnderTest.getInstructionField();
+		
+		assertEquals("Not equal",expectedOutput,actualOutput);
+	}
+
+	
+}
